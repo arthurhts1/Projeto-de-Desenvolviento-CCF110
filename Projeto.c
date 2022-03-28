f30e1b404a21a2ea56add213f976cc15d1d5e2d3
Arthur Teixeira <76131908+arthurhts1@users.noreply.github.com>
2022-03-27 23:33:43 -0300

    Update Projeto.c
    Na tentativa de aprimorar o código colocando structs, funções e arquivos, principalmente arquivos, acabei me perdendo no projeto e acabei não conseguindo realizar todas as alterações. Adicionei o que consegui ate o momento da entrega.

--- Projeto.c
+++ Projeto.c
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct cadastro{
+    int id;
+    char nome[20];
+};
+
+void vendaSetor1 (void);
+void vendaSetor2 (void);
+void vendaSetor3 (void);
+void vendaSetor4 (void);
+
 
 int main()
 {
@@ -10,6 +22,7 @@ int main()
     int setor,contTorcedor=0, vendasetor1=0, vendasetor2=0, vendasetor3=0, vendasetor4=0,n=2000, totalvendido=0;;
     int setor1[20][40],setor2[10][20],setor3[20][40],setor4[10][20];
     int *idsTorcedor;
+    struct cadastro torcedor[2000];
 
 
     idsTorcedor = malloc(contTorcedor * sizeof(int));
@@ -29,11 +42,7 @@ int main()
         for (j=0;j<40;j++){
             setor1[i][j] = 0;
             setor2[i][j] = 0;
-        }
-    }
-    for (i=0;i<10;i++){
-        for (j=0;j<20;j++){
-            setor2[i][j] = 0;
+            setor3[i][j] = 0;
             setor4[i][j] = 0;
         }
     }
@@ -53,17 +62,7 @@ int main()
                 printf("\n*****SETOR 1******\n\n");
                 while (op != 0){
 
-                    printf("Digite o id: ");
-                    scanf("%d",&id);
-
-                    printf("Escolha a linha e a coluna do ingresso: \n");
-                    printf("Linha (0-20): ");
-                    scanf("%d",&i);
-                    printf("Coluna (0-40): ");
-                    scanf("%d",&j);
-
-                    setor1[i][j] = id;
-                    vendasetor1++;
+                    vendaSetor1 ();
 
                     printf("[0] Para sair [1] Para comprar outro ingresso\n");
                     printf("Comando: ");
@@ -74,17 +73,7 @@ int main()
                 printf("\n*****SETOR 2******\n\n");
                 while (op != 0){
 
-                    printf("Digite o id: ");
-                    scanf("%d",&id);
-
-                    printf("Escolha a linha e a coluna do ingresso: \n");
-                    printf("Linha (0-20): ");
-                    scanf("%d",&i);
-                    printf("Coluna (0-40): ");
-                    scanf("%d",&j);
-
-                    setor2[i][j] = id;
-                    vendasetor2++;
+                    vendaSetor2 ();
 
                     printf("[0] Para sair [1] Para comprar outro ingresso\n");
                     printf("Comando: ");
@@ -95,17 +84,7 @@ int main()
                 printf("\n*****SETOR 3******\n\n");
                 while (op != 0){
 
-                    printf("Digite o id: ");
-                    scanf("%d",&id);
-
-                    printf("Escolha a linha e a coluna do ingresso: \n");
-                    printf("Linha (0-20): ");
-                    scanf("%d",&i);
-                    printf("Coluna (0-40): ");
-                    scanf("%d",&j);
-
-                    setor3[i][j] = id;
-                    vendasetor3++;
+                    vendaSetor3 ();
 
                     printf("[0] Para sair [1] Para comprar outro ingresso\n");
                     printf("Comando: ");
@@ -116,17 +95,7 @@ int main()
                 printf("\n*****SETOR 4******\n\n");
                 while (op != 0){
 
-                    printf("Digite o id: ");
-                    scanf("%d",&id);
-
-                    printf("Escolha a linha e a coluna do ingresso: \n");
-                    printf("Linha (0-20): ");
-                    scanf("%d",&i);
-                    printf("Coluna (0-40): ");
-                    scanf("%d",&j);
-
-                    setor4[i][j] = id;
-                    vendasetor4++;
+                    vendaSetor4 ();
 
                     printf("[0] Para sair [1] Para comprar outro ingresso\n");
                     printf("Comando: ");
@@ -149,6 +118,8 @@ int main()
         }
     }
 
+
+
     printf("Ingressos vendidos setor 1: %d\n", vendasetor1);
     printf("Ingressos vendidos setor 2: %d\n", vendasetor2);
     printf("Ingressos vendidos setor 3: %d\n", vendasetor3);
@@ -163,3 +134,74 @@ int main()
 
     return 0;
 }
+
+void vendaSetor1 (void){
+
+    int id,i,j,setor1[20][40],vendasetor1=0,contTorcedor,idsTorcedor;
+    struct cadastro torcedor[2000];
+
+    printf("Digite o id: ");
+    scanf("%d",&torcedor[id].id);
+    printf("Digite o Nome: ");
+    fflush(stdin);
+    gets(torcedor[id].nome);
+
+
+    printf("Escolha a linha e a coluna do ingresso: \n");
+    printf("Linha (0-20): ");
+    scanf("%d",&i);
+    printf("Coluna (0-40): ");
+    scanf("%d",&j);
+
+    setor1[i][j] = id;
+    vendasetor1++;
+}
+void vendaSetor2 (void){
+
+    int id,i,j,setor2[20][40],vendasetor2=0;
+
+    printf("Digite o id: ");
+    scanf("%d",&id);
+
+    printf("Escolha a linha e a coluna do ingresso: \n");
+    printf("Linha (0-20): ");
+    scanf("%d",&i);
+    printf("Coluna (0-40): ");
+    scanf("%d",&j);
+
+    setor2[i][j] = id;
+    vendasetor2++;
+}
+void vendaSetor3 (void){
+
+    int id,i,j,setor3[20][40],vendasetor3=0;
+
+    printf("Digite o id: ");
+    scanf("%d",&id);
+
+    printf("Escolha a linha e a coluna do ingresso: \n");
+    printf("Linha (0-20): ");
+    scanf("%d",&i);
+    printf("Coluna (0-40): ");
+    scanf("%d",&j);
+
+    setor3[i][j] = id;
+    vendasetor3++;
+}
+void vendaSetor4 (void){
+
+    int id,i,j,setor4[20][40],vendasetor4=0;
+
+    printf("Digite o id: ");
+    scanf("%d",&id);
+
+    printf("Escolha a linha e a coluna do ingresso: \n");
+    printf("Linha (0-20): ");
+    scanf("%d",&i);
+    printf("Coluna (0-40): ");
+    scanf("%d",&j);
+
+    setor4[i][j] = id;
+    vendasetor4++;
+}
+
